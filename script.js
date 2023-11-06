8c67c681da44737a2fa80760330cb4fd203f0519
Raul Melo <raulvitor123456@hotmail.com>
2023-11-06 20:21:34 -0300

    #

---
+++ script.js
@@ -0,0 +1,433 @@
+const doces = [
+
+    {
+        id:   "1"   ,
+        nome:  "Confeito Goma"  ,
+        preço:  "5.49" ,
+        descrição: "43cm Barion 140g"  ,
+        imagem:   "https://docefesta.cdn.plataformaneo.com.br/produto/multifotos/hd/21566-MAIN_21566-MAIN.png" ,
+
+    },
+
+    {
+        id:   "2"   ,
+        nome:  "Doce Canudo de Leite"  ,
+        preço:  "9.90" ,
+        descrição:   "6 unidades",
+        imagem:   "https://docefesta.cdn.plataformaneo.com.br/produto/20230411115446_2530997470_D.png"  ,
+    },
+
+    {
+        id:    "3"  ,
+        nome:  "Geladinho"  ,
+        preço:  "12.80" ,
+        descrição:  "40 unidades" ,
+        imagem:     "https://docefesta.cdn.plataformaneo.com.br/produto/1953-MAIN.png"  ,
+    },
+
+    {    
+        id:    "4"  ,
+        nome:   "Goma Deliket" ,
+        preço:  "13.90" ,
+        descrição:  "30un x 16g" ,
+        imagem:      "https://docefesta.cdn.plataformaneo.com.br/produto/20230125155518_8420991580_D.png"   ,
+
+    },
+
+    {
+        id:    "5"  ,
+        nome:  "Jujuba "  ,
+        preço:  "11.20" ,
+        descrição:  "1 Kg" ,
+        imagem:      "https://docefesta.cdn.plataformaneo.com.br/produto/20230919165039_1316998684_D.png"   ,
+
+    },
+
+    {
+        id:  "6"    ,
+        nome:  "Doce paçoca"  ,
+        preço:  "15.90" ,
+        descrição:  "900g 50 unidades" ,
+        imagem:    "https://docefesta.cdn.plataformaneo.com.br/produto/20230414084301_7683992317_D.png"   ,
+
+    },
+
+    {
+        id:  "7"    ,
+        nome:   "Marshmallow Maxmallows" ,
+        preço: "10.40"  ,
+        descrição: "250g"  ,
+        imagem:    "https://docefesta.cdn.plataformaneo.com.br/produto/multifotos/hd/21806-MAIN.png"   ,
+
+    },
+
+    {
+        id:   "8"   ,
+        nome:   "Goma Tubes Twister Citrico" ,
+        preço:  "6.20" ,
+        descrição: "80g"  ,
+        imagem:    "https://docefesta.cdn.plataformaneo.com.br/produto/multifotos/hd/20230127210718_9867990133_DZ.png"      ,
+
+    },
+
+    {
+        id:   "9"   ,
+        nome:  "Paçoca choquita"  ,
+        preço: "16.10"  ,
+        descrição: "144g"  ,
+        imagem:   "https://docefesta.cdn.plataformaneo.com.br/produto/21711-MAIN_21711-MAIN.png"    ,
+
+    },
+    {
+        id:   "10"   ,
+        nome:   "Chocolate kit kat " ,
+        preço:  "41.65" ,
+        descrição: "42un 41,5g"  ,
+        imagem:   "https://images.tcdn.com.br/img/img_prod/791033/chocolate_kit_kat_nestle_41_5g_com_24_unidades_5473_1_143db410f15547994105a24116319647.jpg"    ,
+
+    }, {
+        id:   "11"   ,
+        nome:   "Pacote ouro branco" ,
+        preço: "46.50"  ,
+        descrição: "1kg "  ,
+        imagem:    "https://images.tcdn.com.br/img/img_prod/791033/chocolate_bombom_ouro_branco_mondelez_1kg_375_1_af9eddd4a251b543952a9dbdab208c8a.jpg"     ,
+
+    }, {
+        id:   "12"   ,
+        nome:  "Chiclete Plutonita  "  ,
+        preço:  "8.49" ,
+        descrição:  "180g" ,
+        imagem:      "https://images.tcdn.com.br/img/img_prod/791033/chiclete_plutonita_arcor_cabeca_abobora_180g_8007_1_f1775fb5ba35528140190257ddb8232c.jpg"    ,
+
+    }, {
+        id:   "13"   ,
+        nome: "Bala 7 Belo Framboesa"   ,
+        preço:  "11.59" ,
+        descrição:  "500g" ,
+        imagem:      "https://images.tcdn.com.br/img/img_prod/791033/bala_7_belo_framboesa_arcor_500g_7993_1_8e12b7e86ae4542c0d952181ea66b930.jpg"   ,
+
+    }, {
+        id:    "14"  ,
+        nome:   "Tablete Alpino Ao Leite" ,
+        preço:  "6.79" ,
+        descrição:   "90g",
+        imagem:   "https://images.tcdn.com.br/img/img_prod/791033/tablete_alpino_ao_leite_nestle_90g_7767_1_435b9bc022be47feb0c16e598183312a.jpg"  ,
+
+    }, {
+        id:   "15"   ,
+        nome:  "Chocolate Tortuguita"  ,
+        preço:  "25.80" ,
+        descrição:  "24un" ,
+        imagem:      "https://images.tcdn.com.br/img/img_prod/791033/chocolate_tortuguita_brigadeiro_arcor_com_24_unidades_7193_1_7311042baa8e6b790f9e6a1b9c8e685a.jpg"  ,
+
+    }, {
+        id:    "16"  ,
+        nome:  "Chocolate Unidade M&MS"  ,
+        preço: "7.20"  ,
+        descrição:  "80g" ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/chocolate_unidade_mems_chocolate_80g_4873_1_10348f99ddc3dd343a16885e05ba5c68.jpg"   ,
+
+    }, {
+        id:   "17"   ,
+        nome:  "BUBBALOO Tutti Frutti "  ,
+        preço: "15.49"  ,
+        descrição: "60un"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/bubbaloo_tutti_frutti_c_60_unidades_3493_1_617b85c0f3c4ed7521414ad72b306c7f.jpg"    ,
+
+    },{
+        id:   "18"   ,
+        nome:  "Freegells Gum Tutti Frutti "  ,
+        preço: "14.49"  ,
+        descrição: "14un"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/freegells_gum_original_tutti_frutti_riclan_c_15_unidades_3423_1_c0a0fe71b0652b5f27d7ce6b02767c97.jpg"    ,
+
+    },{
+        id:   "19"   ,
+        nome:  "Mentos Pure Stick Mint "  ,
+        preço: "24.25"  ,
+        descrição: "14un"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/mentos_pure_stick_mint_c_14_unidades_3387_1_610c141b739a8e7de48123e07cb37e57.jpg"    ,
+
+    },{
+        id:   "20"   ,
+        nome:  "Drops Halls Xs Mini Cereja "  ,
+        preço: "15.49"  ,
+        descrição: "18un 15g"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/drops_halls_xs_mini_cereja_c_18_unidades_15g_3263_1_4f297520d8230c34031766b71187eede.jpg"    ,
+
+    },{
+        id:   "21"   ,
+        nome:  "Cacau Nutella "  ,
+        preço: "27.99"  ,
+        descrição: "350g"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/creme_de_avela_com_cacau_nutella_350g_2531_1_a08796ed2c051904fca36c269a213306.jpg"    ,
+
+    },{
+        id:   "22"   ,
+        nome:  "Drops Azedinha Morango"  ,
+        preço: "7.70"  ,
+        descrição: "12un"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/drops_icekiss_azedinha_morango_cory_c_12_und_1053_1_8f97091c423f86b6b16db36c442ffcf9.jpg"    ,
+
+    },{
+        id:   "23"   ,
+        nome:  "Chocolate Bis Branco-Lacta "  ,
+        preço: "7.49"  ,
+        descrição: "100g"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/chocolate_bis_branco_lacta_387_1_ec78aa8a765a58c115042e94d0bf4a4a.jpg"    ,
+
+    },{
+        id:   "24"   ,
+        nome:  "Garoto bombons "  ,
+        preço: "12.49"  ,
+        descrição: "250g"  ,
+        imagem:     "https://images.tcdn.com.br/img/img_prod/791033/chocolate_caixa_de_bombons_sortidos_garoto_250g_373_1_63f3617dd1fd422de640a2e1c2c20bd7.jpg"    ,
+
+    },{
+        id:   "25"   ,
+        nome:  "Marmelada caixa "  ,
+        preço: "32.20"  ,
+        descrição: "400g"  ,
+        imagem:     "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQFSzSzR4dgU-okHriWmkkqfHfRfLcjU1imG5iKswPDM0BnZ7IS_SMiZd6Z9jxV_EXtoH9GJEUHM4qUzUNAQkelETOAX10RrtEkZo0HNOFOf2t9CshIf4HwOPc&usqp=CAE"    ,
+
+    }
+    
+
+]
+
+const carrinho = [];
+
+function adicionarAoCarrinho(doce) {
+    // Vendo se tá no carrinho
+    const itemIndex = carrinho.findIndex((item) => item.id === doce.id);
+    
+    if (itemIndex !== -1) {
+        // Se está aumenta 1(logica de adicionar 1 na quantidade)
+        carrinho[itemIndex].quantidade += 1;
+    } else {
+        // Quando adicionar no carrinho já aparecer 1 na quant
+        doce.quantidade = 1;
+        carrinho.push(doce);
+    }
+    atualizarCarrinho();
+}
+
+let total = 0;
+  
+  function atualizarCarrinho() {
+ const carrinhoContainer = document.getElementById("carrinho-container");
+    carrinhoContainer.innerHTML = "";
+
+
+
+//criando os itens do carrinho
+    carrinho.forEach((item) => {
+        const itemCarrinho = document.createElement("div");
+        itemCarrinho.className = "carrinho-item";
+
+        const itemImagemContainer = document.createElement("div");
+        itemImagemContainer.className = "carrinho-imagem";
+
+        const itemImagem = document.createElement("img");
+        itemImagem.src = item.imagem;
+        itemImagemContainer.appendChild(itemImagem);
+
+        const itemInfoContainer = document.createElement("div");
+        itemInfoContainer.className = "carrinho-info";
+
+        const itemNome = document.createElement("p");
+        itemNome.textContent = item.nome;
+        const itemPreco = document.createElement("p");
+        itemPreco.textContent = `R$${item.preço}`;
+
+        // span para exibir número
+        const quantidadeSpan = document.createElement("span");
+        quantidadeSpan.textContent = item.quantidade;
+
+               //botão do mais
+               const botaoAumentar = document.createElement("button");
+               botaoAumentar.textContent = "+";
+               botaoAumentar.addEventListener("click", () => {
+                   item.quantidade += 1;
+                   quantidadeSpan.textContent = item.quantidade;
+                   itemPreco.textContent = `R$${(item.preço * item.quantidade).toFixed(2)}`;
+                   total = calcularTotal();
+                   atualizarCarrinho();
+               });
+       
+               //botão do menos 
+   const botaoDiminuir = document.createElement("button");
+               botaoDiminuir.textContent = "-";
+               botaoDiminuir.addEventListener("click", () => {
+                   if (item.quantidade > 1) {
+                       item.quantidade -= 1;
+                       quantidadeSpan.textContent = item.quantidade;
+                       itemPreco.textContent = `R$${(item.preço * item.quantidade).toFixed(2)}`;
+                       total = calcularTotal();
+                       atualizarCarrinho();
+                   } else {
+                       // Se a quantidade descer pra 0, excluir o item do carrinho fé
+                       carrinho.splice(carrinho.indexOf(item), 1);
+                       total = calcularTotal();
+                       atualizarCarrinho();
+                   }
+               });
+
+        itemInfoContainer.appendChild(itemNome);
+        itemInfoContainer.appendChild(itemPreco);
+//appendChild para adicionar ao pai o filho
+        
+        itemInfoContainer.appendChild(quantidadeSpan);
+
+        itemInfoContainer.appendChild(botaoAumentar);
+        itemInfoContainer.appendChild(botaoDiminuir);
+
+        itemCarrinho.appendChild(itemImagemContainer);
+        itemCarrinho.appendChild(itemInfoContainer);
+
+        carrinhoContainer.appendChild(itemCarrinho);
+
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "Excluir";
+        botaoExcluir.addEventListener("click", () => {
+            carrinho.splice(carrinho.indexOf(item), 1); // Splice pra remover
+            total = calcularTotal(); // atualiza o preçp
+            atualizarCarrinho(); // Atualiza o carrinho
+        });
+        itemInfoContainer.appendChild(botaoExcluir);
+
+    });
+
+ 
+    const totalCarrinho = document.getElementById("total-carrinho");
+    totalCarrinho.textContent = `Total: R$${total.toFixed(2)}`;
+
+
+// Vê se o carrinho está vazio para "zerar" o preço
+if (carrinho.length === 0) {
+totalCarrinho.textContent = `Total: R$0.00`;
+}
+    
+}
+
+//criando os elementos principais
+for (const doce of doces) {
+    const cardDoce = document.createElement("div");
+    cardDoce.className = "card";
+    cardDoce.innerHTML = `
+        <img src="${doce.imagem}" alt="${doce.nome}">
+        <p class = "textoNome">${doce.nome}</p>
+        <p class = "textoDescricao">${doce.descrição}</p>
+        <p class = "textoPreco">R$${doce.preço}</p>
+        <button class="botaoAdicionar">Adicionar</button>
+    `;
+
+    cardDoce.querySelector(".botaoAdicionar").addEventListener("click", () => {
+        adicionarAoCarrinho(doce); // Chama a função adicionarAoCarrinho
+    
+        // Atualize o preço total
+        total = calcularTotal();
+        atualizarCarrinho();
+    });
+
+    document.getElementById("container-doce").appendChild(cardDoce);
+}
+
+
+  const carrinhoIcon = document.getElementById("carrinho");
+  const barraCarrinho = document.getElementById("barraCarrinho");
+  const fecharCarrinho = document.getElementById("fecharCarrinho");
+  
+  carrinhoIcon.addEventListener("click", () => {
+    barraCarrinho.classList.add("aberto"); // "abre" o carrinho
+  });
+  
+  fecharCarrinho.addEventListener("click", () => {
+    barraCarrinho.classList.remove("aberto"); // Fecha o carrinho
+  });
+
+  
+  function calcularTotal() {
+    return carrinho.reduce((acc, item) => acc + (item.preço * item.quantidade), 0); // calular o preço dos itens
+}
+
+
+
+function enviarFormulario(event) {
+    event.preventDefault(); // Impede o submit do formulário
+
+   //dados para formar o novo elemento
+    const nome = document.getElementById('nome').value;
+    const preço = document.getElementById('preço').value;
+    const descrição = document.getElementById('descrição').value;
+    const imagem = document.getElementById('imagem').value;
+
+    // Validar dados (Ver se realmente faz sentido depois)
+    if (!nome || !preço || !descrição || !imagem) {
+        alert('Preencha todos os campos do formulário.');
+        return;
+    }
+
+    // Atualiza o array de doces
+    const novoDoce = {
+        id: doces.length + 1, // IDDDD
+        nome: nome,
+        preço: preço,
+        descrição: descrição,
+        imagem: imagem
+    };
+    doces.push(novoDoce);
+
+    // Atualiza a exibição dos doces com o novo card
+    criarCardDoce(novoDoce); 
+
+    ocultarFormulario(); // Oculta o formulário
+}
+
+
+document.getElementById('formulario').addEventListener('submit', enviarFormulario);
+
+
+function exibirFormulario() {
+    document.getElementById('overlay').style.display = 'block'; // Exibe o overlay
+    document.getElementById('formulario').style.display = 'block'; // Exibe o formulário 
+}
+
+
+function ocultarFormulario() {
+    document.getElementById('overlay').style.display = 'none'; // Oculta o overlay
+    document.getElementById('formulario').style.display = 'none'; // Oculta o formulário de cadastro
+}
+
+
+document.getElementById('botaoAdicionarProduto').addEventListener('click', exibirFormulario);
+
+function criarCardDoce(doce) {
+    const cardDoce = document.createElement("div");
+    cardDoce.className = "card";
+    cardDoce.innerHTML = `
+        <img src="${doce.imagem}" alt="${doce.nome}">
+        <p class = "textoNome">${doce.nome}</p>
+        <p class = "textoDescricao">${doce.descrição}</p>
+        <p class = "textoPreco">R$${doce.preço}</p>
+        <button class="botaoAdicionar">Adicionar</button>
+    `;
+
+    cardDoce.querySelector(".botaoAdicionar").addEventListener("click", () => {
+        adicionarAoCarrinho(doce); // Chama a função adicionarAoCarrinho, igual anteriormente 
+    
+        // Atualize o preço total 
+        total = calcularTotal();
+        atualizarCarrinho();
+    });
+
+    document.getElementById("container-doce").appendChild(cardDoce);
+    container.appendChild(cardDoce);
+
+    
+   // window.getComputedStyle(container);
+  
+}
+document.getElementById("fecharFormulario").addEventListener("click", () => {
+    ocultarFormulario();
+});
